820dfe39503d2c9e6a494cdbc65f423641456725
rodrigo-v4z <rodrigovazd.oliveira@gmail.com>
2025-07-30 10:44:30 -0300

    Versão com variávais protegidas

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,19 @@ import { Usuario } from './models/Usuario'
 function App() {
 
   const u1 = new Usuario('João da Silva', 37);
+  const u2 = new Usuario('Maria Oliveira', 25);
 
   console.log(u1.apresentar());
+
+  u1.nome = 'Jorge';
 
   return (
     <>
       <p>
-        {u1.idade} anos
+        {u1.apresentar()}
+      </p>
+      <p>
+        {u2.apresentar()}
       </p>
     </>
   )
