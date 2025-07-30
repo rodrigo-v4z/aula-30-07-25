594c3b19fac45bd24c79235b9b6ddbeba0bbe22f
rodrigo-v4z <rodrigovazd.oliveira@gmail.com>
2025-07-30 10:17:01 -0300

    Versãpo com classe usuário

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import { Usuario } from './models/Usuario'
 
 function App() {
 
+  const u1 = new Usuario('João da Silva', 37);
+
+  console.log(u1.apresentar());
+
   return (
     <>
-      
+      <p>
+        {u1.idade} anos
+      </p>
     </>
   )
 }
